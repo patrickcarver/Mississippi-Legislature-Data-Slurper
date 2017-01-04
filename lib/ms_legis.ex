1b932a35a7e7e90629f1cfdb469774b9ebbb2536
Patrick Carver <patrickcarver@gmail.com>
2017-01-03 20:50:43 -0600

    read in and clean up xml
    Get xml from the webpage. Had to remove xml metadata, endline chars,
    and tab chars to get it SweetXml to parse it.

--- lib/ms_legis.ex
+++ lib/ms_legis.ex
@@ -1,8 +1,17 @@
 defmodule MsLegis do
+
   def run do
     IO.puts "--- Started ---"
+    xml_top = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><?xml-stylesheet type=\"text/xsl\" href=\"dtds_xslts/memberlist.xslt\"?> <!DOCTYPE LEGISLATURE SYSTEM \"dtds_xslts/memberlist.dtd\"> "
     house_url = "http://billstatus.ls.state.ms.us/members/hr_membs.xml"
-    
+    response = HTTPotion.get house_url
+    xml = response.body
+          |> String.replace("\r", "")
+          |> String.replace("\n", "")
+          |> String.replace("\t", "")
+          |> String.replace(xml_top, "")
+    IO.puts xml
+
     IO.puts "--- Finished ---"
   end
 end
