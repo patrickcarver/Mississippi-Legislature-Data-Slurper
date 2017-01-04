1cd381b25d80815455362da78c2fed7d781d150a
Patrick Carver <patrickcarver@gmail.com>
2017-01-03 19:47:24 -0600

    started on main function

--- lib/ms_legis.ex
+++ lib/ms_legis.ex
@@ -1,2 +1,8 @@
 defmodule MsLegis do
+  def run do
+    IO.puts "--- Started ---"
+    house_url = "http://billstatus.ls.state.ms.us/members/hr_membs.xml"
+    
+    IO.puts "--- Finished ---"
+  end
 end
