2b94f4307a4481957346b85e620e6582b5fb7509
Patrick Carver <patrickcarver@gmail.com>
2017-01-03 21:11:33 -0600

    moved xml cleanup to function, print chairman name

--- lib/ms_legis.ex
+++ lib/ms_legis.ex
@@ -1,17 +1,24 @@
 defmodule MsLegis do
+  import SweetXml
 
   def run do
     IO.puts "--- Started ---"
-    xml_top = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><?xml-stylesheet type=\"text/xsl\" href=\"dtds_xslts/memberlist.xslt\"?> <!DOCTYPE LEGISLATURE SYSTEM \"dtds_xslts/memberlist.dtd\"> "
     house_url = "http://billstatus.ls.state.ms.us/members/hr_membs.xml"
     response = HTTPotion.get house_url
-    xml = response.body
-          |> String.replace("\r", "")
-          |> String.replace("\n", "")
-          |> String.replace("\t", "")
-          |> String.replace(xml_top, "")
-    IO.puts xml
+    xml = response.body |> clean_xml
+
+    chairman_name = xml |> xpath(~x"//CHAIR_NAME/text()")
+    IO.puts chairman_name
 
     IO.puts "--- Finished ---"
+  end
+
+  defp clean_xml(text) do
+    xml_top = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><?xml-stylesheet type=\"text/xsl\" href=\"dtds_xslts/memberlist.xslt\"?> <!DOCTYPE LEGISLATURE SYSTEM \"dtds_xslts/memberlist.dtd\"> "
+    text
+      |> String.replace("\r", "")
+      |> String.replace("\n", "")
+      |> String.replace("\t", "")
+      |> String.replace(xml_top, "")
   end
 end
