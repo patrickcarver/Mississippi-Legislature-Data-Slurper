ec922253a6590c223728cb8b957e92813dd7c153
Patrick Carver <patrickcarver@gmail.com>
2017-01-04 20:18:02 -0600

    Refactor link list creation to function

--- lib/ms_legis.ex
+++ lib/ms_legis.ex
@@ -3,10 +3,15 @@ defmodule MsLegis do
 
   def run do
     IO.puts "--- Started ---"
+
     house_url = "http://billstatus.ls.state.ms.us/members/hr_membs.xml"
     response = HTTPotion.get house_url
-    xml = response.body |> clean_xml
+    links_list = response.body |> clean_xml |> create_list
 
+    IO.puts "--- Finished ---"
+  end
+
+  def create_list(xml) do
     chair_link = xml |> xpath(~x"//CHAIR_LINK/text()")
     protemp_link = xml |> xpath(~x"//PROTEMP_LINK/text()")
 
@@ -18,9 +23,7 @@ defmodule MsLegis do
     m4_links = xml |> xpath(~x"//MEMBER/M4_LINK/text()"l)
     m5_links = xml |> xpath(~x"//MEMBER/M5_LINK/text()"l)
 
-    links_list = officer_links ++ m1_links ++ m2_links ++ m3_links ++ m4_links ++ m5_links
-
-    IO.puts "--- Finished ---"
+    officer_links ++ m1_links ++ m2_links ++ m3_links ++ m4_links ++ m5_links
   end
 
   defp clean_xml(text) do
