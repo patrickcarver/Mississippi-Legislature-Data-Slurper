c9a3ccbf18f2ef652fddd2ca63a604ea3593d116
Patrick Carver <patrickcarver@gmail.com>
2017-01-07 12:56:17 -0600

    create tests for xml metadata

--- test/ms_legis_test.exs
+++ test/ms_legis_test.exs
@@ -2,7 +2,15 @@ defmodule MsLegisTest do
   use ExUnit.Case
   doctest MsLegis
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "no change in list xml metadata" do
+    metadata = %MsLegis.XmlMetadata{}
+    list_xml_metadata = ~s(<?xml version="1.0" encoding="ISO-8859-1"?><?xml-stylesheet type="text/xsl" href="dtds_xslts/memberlist.xslt"?> <!DOCTYPE LEGISLATURE SYSTEM "dtds_xslts/memberlist.dtd">)
+    assert list_xml_metadata == metadata.list
+  end
+
+  test "no change in member xml metadata" do
+    metadata = %MsLegis.XmlMetadata{}
+    member_xml_metadata = ~s(<?xml version="1.0" encoding="ISO-8859-1"?><?xml-stylesheet type="text/xsl" href="../dtds_xslts/house.xslt"?><!DOCTYPE MEMBINFO SYSTEM "../dtds_xslts/membinfo.dtd">)
+    assert member_xml_metadata == metadata.member
   end
 end
