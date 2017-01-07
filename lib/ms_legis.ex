b84d1b1821ecf662fbc9be053ab3a4e48e9ff2fc
Patrick Carver <patrickcarver@gmail.com>
2017-01-07 12:11:59 -0600

    refactor cleanxml functions to module

--- lib/ms_legis.ex
+++ lib/ms_legis.ex
@@ -1,30 +1,57 @@
 defmodule MsLegis do
   import SweetXml
 
+  defmodule XmlMetadata do
+    defstruct list:   ~s(<?xml version="1.0" encoding="ISO-8859-1"?><?xml-stylesheet type="text/xsl" href="dtds_xslts/memberlist.xslt"?> <!DOCTYPE LEGISLATURE SYSTEM "dtds_xslts/memberlist.dtd">),
+              member: ~s(<?xml version="1.0" encoding="ISO-8859-1"?><?xml-stylesheet type="text/xsl" href="../dtds_xslts/house.xslt"?><!DOCTYPE MEMBINFO SYSTEM "../dtds_xslts/membinfo.dtd">)
+  end
+
+  defmodule CleanXml do
+    def apply(text, metadata) do
+      text
+      |> remove_whitespace
+      |> String.replace(metadata, "")
+    end
+
+    def remove_whitespace(text) do
+      text
+      |> String.replace("\r", "")
+      |> String.replace("\n", "")
+      |> String.replace("\t", "")
+    end
+  end
+
   def run do
     IO.puts "--- Started ---"
 
+    xml_metadata = %XmlMetadata{}
+
     base_url = "http://billstatus.ls.state.ms.us/members/"
-
     house_url = base_url <> "hr_membs.xml"
-    response = HTTPotion.get house_url
 
-    links_list = response.body |> clean_list_xml |> create_list
+    response = HTTPotion.get house_url
+    links_list = response.body |> get_list_xml(xml_metadata.list)
 
     for link <- links_list do
       member_link = base_url <> List.to_string(link)
-      get_member_xml(member_link)
+      get_member_xml(member_link, xml_metadata.member)
     end
 
     IO.puts "--- Finished ---"
   end
 
-  def get_member_xml(link_url) do
-    response = HTTPotion.get link_url
-    IO.puts response.body |> clean_member_xml |> xpath(~x"//PARTY/text()")
+  def get_list_xml(body, metadata) do
+    body
+    |> CleanXml.apply(metadata)
+    |> create_list
   end
 
-
+  def get_member_xml(link_url, metadata) do
+    response = HTTPotion.get link_url
+    IO.puts response.body
+            |> CleanXml.apply(metadata)
+            |> xpath(~x"//PARTY/text()")
+  end
 
   def create_list(xml) do
     chair_link = xml |> xpath(~x"//CHAIR_LINK/text()")
@@ -43,24 +70,4 @@ defmodule MsLegis do
 
 
 
-  defp clean_list_xml(text) do
-    xml_top = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><?xml-stylesheet type=\"text/xsl\" href=\"dtds_xslts/memberlist.xslt\"?> <!DOCTYPE LEGISLATURE SYSTEM \"dtds_xslts/memberlist.dtd\"> "
-    text
-    |> remove_whitespace
-    |> String.replace(xml_top, "")
-  end
-
-  defp clean_member_xml(text) do
-    xml_top = ~s(<?xml version="1.0" encoding="ISO-8859-1"?><?xml-stylesheet type="text/xsl" href="../dtds_xslts/house.xslt"?><!DOCTYPE MEMBINFO SYSTEM "../dtds_xslts/membinfo.dtd">)
-    text
-    |> remove_whitespace
-    |> String.replace(xml_top, "")
-  end
-
-  defp remove_whitespace(text) do
-    text
-    |> String.replace("\r", "")
-    |> String.replace("\n", "")
-    |> String.replace("\t", "")
-  end
 end
