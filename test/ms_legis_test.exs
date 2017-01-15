b50c3077e0d3a7d21a096f8e4fdff02e28987cbd
Patrick Carver <patrickcarver@gmail.com>
2017-01-15 16:02:19 -0600

    test remove white space with one function

--- test/ms_legis_test.exs
+++ test/ms_legis_test.exs
@@ -6,28 +6,14 @@ defmodule MsLegisTest do
     {:ok, []}
   end
 
-  test "test remove newline", state do
-    original = "Hello World\r\n"
-    cleaned = MsLegis.CleanXml.remove_whitespace(original)
-
-    assert cleaned == "Hello World"
-  end
-
-  test "test remove tab", state do
-    original = "Hello \tWorld"
-    cleaned = MsLegis.CleanXml.remove_whitespace(original)
-
-    assert cleaned == "Hello World"
-  end
-
-  test "test remove newline and tab", state do
+  test "test remove white space" do
     original = "Hello \tWorld\r\n"
     cleaned = MsLegis.CleanXml.remove_whitespace(original)
 
     assert cleaned == "Hello World"
   end
 
-  test "test remove bad quotes", state do
+  test "test remove bad quotes" do
     original = <<147>> <> "Hello World" <> <<148>>
     cleaned = MsLegis.CleanXml.remove_bad_quotes(original)
 
