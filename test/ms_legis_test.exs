f15d71ebc7d07673fe25e7f2df0bac03d7cf1b58
Patrick Carver <patrickcarver@gmail.com>
2017-01-15 15:49:06 -0600

    add tests for CleanXml

--- test/ms_legis_test.exs
+++ test/ms_legis_test.exs
@@ -2,12 +2,29 @@ defmodule MsLegisTest do
   use ExUnit.Case
   doctest MsLegis
 
-
-
-  test "test chair link" do
-    import MsLegis.XQuery
-
-    xquery = %MsLegis.XQuery{}
-    assert xquery.chair_link == "//CHAIR_LINK/text()"
+  setup_all do
+    {:ok, []}
   end
+
+  test "test remove newline", state do
+    original = "Hello World\r\n"
+    cleaned = MsLegis.CleanXml.remove_whitespace(original)
+
+    assert cleaned == "Hello World"
+  end
+
+  test "test remove tab", state do
+    original = "Hello \tWorld"
+    cleaned = MsLegis.CleanXml.remove_whitespace(original)
+
+    assert cleaned == "Hello World"
+  end
+
+  test "test remove newline and tab", state do
+    original = "Hello \tWorld\r\n"
+    cleaned = MsLegis.CleanXml.remove_whitespace(original)
+
+    assert cleaned == "Hello World"
+  end
+
 end
