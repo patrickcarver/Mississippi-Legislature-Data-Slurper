b701e36aa6612b1abcba123b51597a824a277798
Patrick Carver <patrickcarver@gmail.com>
2017-01-15 19:54:22 -0600

    alias modules in test

--- test/ms_legis_test.exs
+++ test/ms_legis_test.exs
@@ -2,6 +2,7 @@ defmodule MsLegisTest do
   use ExUnit.Case
   doctest MsLegis
 
+  alias MsLegis.{CleanXml, GetXmlFromUrl, XmlMetadata, Urls}
 
 
   setup_all do
@@ -11,21 +12,21 @@ defmodule MsLegisTest do
 
   test "remove white space" do
     original = "Hello \tWorld\r\n"
-    cleaned = MsLegis.CleanXml.remove_whitespace(original)
+    cleaned = CleanXml.remove_whitespace(original)
 
     assert cleaned == "Hello World"
   end
 
   test "remove bad quotes" do
     original = <<147>> <> "Hello World" <> <<148>>
-    cleaned = MsLegis.CleanXml.remove_bad_quotes(original)
+    cleaned = CleanXml.remove_bad_quotes(original)
 
     assert cleaned == "Hello World"
   end
 
   test "retrieve 122 house member links" do
-    list = MsLegis.GetXmlFromUrl.apply(MsLegis.Urls.house_link)
-            |> MsLegis.get_list_xml(MsLegis.XmlMetadata.list)
+    list = GetXmlFromUrl.apply(Urls.house_link)
+            |> MsLegis.get_list_xml(XmlMetadata.list)
 
     assert Enum.count(list) == 122
   end
