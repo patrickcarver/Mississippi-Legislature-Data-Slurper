245135840b8702b5ceb954fb1edf0dc21a2abc05
Patrick Carver <patrickcarver@gmail.com>
2017-01-21 20:32:58 -0600

    start parsing name

--- lib/process_member_name.ex
+++ lib/process_member_name.ex
@@ -20,12 +20,39 @@ defmodule MsLegis.ProcessMemberName do
     name |> remove_commas |> remove_periods
   end
 
-  def is_suffix(value) do
+  def is_suffix?(value) do
     value in @suffixes
   end
 
+  def get_suffix(value) do
+    if is_suffix?(name.last) do
+      name.last
+    else
+      ""
+    end
+  end
+
   def get_struct(name) do
-    # is last in suffixes?
-    
+    first_name = name.first
+    middle_name = ""
+    nick_name = ""
+    last_name = ""
+
+    suffix = get_suffix(name.last)
+
+    if suffix = "" do
+      last_name = name.last
+    end
+
+
+
+    %MemberName.new({
+      first_name: first_name,
+      middle_name: middle_name,
+      nick_name: nick_name,
+      last_name: last_name,
+      suffix: suffix
+    })
+
   end
 end
