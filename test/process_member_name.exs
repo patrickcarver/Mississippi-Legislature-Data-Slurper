f6031a3e108d1cd0640941e144b7183169df7cc8
Patrick Carver <patrickcarver@gmail.com>
2017-01-22 16:23:41 -0600

    move ProcessMemberName tests to separate file

---
+++ test/process_member_name.exs
@@ -0,0 +1,51 @@
+defmodule ProcessMemberNameTest do
+  use ExUnit.Case
+
+  alias MsLegis.{
+    MemberName,
+    ProcessMemberName
+  }
+
+  test "remove punctuation from Willie J. Perkins, Sr." do
+    original = "Willie J. Perkins, Sr."
+    cleaned = ProcessMemberName.remove_punctuation(original)
+
+    assert cleaned == "Willie J Perkins Sr"
+  end
+
+  test "is Sr suffix?" do
+    assert ProcessMemberName.is_suffix?("Sr")
+  end
+
+  test "is V suffix?" do
+    assert ProcessMemberName.is_suffix?("V")
+  end
+
+  test "is III suffix?" do
+    assert ProcessMemberName.is_suffix?("III")
+  end
+
+  test "is Tullos suffix?" do
+    refute ProcessMemberName.is_suffix?("Tullos")
+  end
+
+  test "create MemberName struct for Willie J. Perkins, Sr., get first name" do
+    member_struct = ProcessMemberName.apply("Willie J. Perkins, Sr.")
+    assert member_struct.first_name == "Willie"
+  end
+
+  test "create MemberName struct for Willie J. Perkins, Sr., get last name" do
+    member_struct = ProcessMemberName.apply("Willie J. Perkins, Sr.")
+    assert member_struct.last_name == "Perkins"
+  end
+
+  test "create MemberName struct for Willie J. Perkins, Sr., get suffix" do
+    member_struct = ProcessMemberName.apply("Willie J. Perkins, Sr.")
+    assert member_struct.suffix == "Sr"
+  end
+
+  test "create MemberName struct for Mark Tullos, get last name" do
+    member_struct = ProcessMemberName.apply("Mark Tullos")
+    assert member_struct.last_name == "Tullos"
+  end
+end
